eb0ad14924b759eee02e1960598003a4cae5f672
oliflox <83953551+oliflox@users.noreply.github.com>
2024-06-04 16:05:54 +0200

    revert

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-// Créer un objet JavaScript avec le HTML du header
-const headerObj = {
-    html: `
-        <header class="header">
-            <a href="index.html" class="header__link">
-                <img class="header__logo" src="../assets/icon/logo.png" alt="FishEye Home page">
-            </a>
-            <h1 class="header__title">Nos photographes</h1>
-        </header>
-    `
-};
-
-document.body.insertAdjacentHTML('afterbegin', headerObj.html);
-
 function createPhotographerCard(photographer) {
     const photographersProfile =
         `<img class="main__grid__card__profile__picture" src="../assets/img/PhotographersID/${photographer.portrait}"
@@ -23,7 +9,7 @@ function createPhotographerCard(photographer) {
         <p class="main__grid__card__info__tagline">${photographer.tagline}</p>
         <p class="main__grid__card__info__price">${photographer.price}€/jour</p>`;
 
-    return `<a class="main__grid__link" href="photographers/${photographer.id}">
+    return `<a class="main__grid__link" href="photographer_page.html">
             <div class="main__grid__card">
                 <div class="main__grid__card__profile">
                     ${photographersProfile}
