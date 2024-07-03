6d7017a3efd15612c0f633503ac3e21d004029d5
oliflox <83953551+oliflox@users.noreply.github.com>
2024-07-03 13:46:51 +0200

    remove tab index

--- js/components/PhotographerProfile.js
+++ js/components/PhotographerProfile.js
@@ -10,7 +10,7 @@ export const render = (photographer) => {
         </a>
     </header>
     <section class="photographer-main__profile" id="photographer-main__profile"> 
-        <div tabindex="0" class="photographer-main__profile__card">
+        <div class="photographer-main__profile__card">
             <h1 class="photographer-main__profile__card__name">${name}</h1>
             <div class="photographer-main__profile__card__info">
                 <p class="photographer-main__profile__card__info__location">${city}, ${country}</p>
@@ -18,7 +18,7 @@ export const render = (photographer) => {
             </div>
         </div>
         <button id="contact-button" class="photographer-main__profile__contact button" alt="Contact me ${name}">Contactez-moi</button>
-        <img tabindex="0" class="photographer-main__profile__portrait" src="../assets/img/PhotographersID/${portrait}" alt="${name} profile's picture">
+        <img class="photographer-main__profile__portrait" src="../assets/img/PhotographersID/${portrait}" alt="${name} profile's picture">
     </section>
     ${ContactModal.contactPopUp(photographer)}
     `;
